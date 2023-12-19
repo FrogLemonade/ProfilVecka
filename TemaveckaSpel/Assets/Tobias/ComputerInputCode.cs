645dfebbe9ccf3eb114ae484d3a9319947136c99
TotusPromus <153913836+TitusPrimus@users.noreply.github.com>
2023-12-19 10:50:12 +0100

    Straight male to male sex
    g

---
+++ TemaveckaSpel/Assets/Tobias/ComputerInputCode.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComputerInputCode : MonoBehaviour
+{
+    public TMP_InputField TerminalInput;
+}
